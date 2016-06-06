ad0a19a12f9d3e6adffcc554732ce8629e71f713
jjchern <jiajia.chern@gmail.com>
2016-06-06 18:46:20 -0500

    Run `devtools::document`

--- R/data.R
+++ R/data.R
@@ -1,8 +1,9 @@
-#' FIPS data
+#' State-level FIPS Code
 #'
 #' FIPS Codes for the States and the District of Columbia form Census.
 #'
-#' @format A data frame with three variables: #' \describe{
+#' @format A data frame with three variables:
+#' \describe{
 #' \item{fips}{FIPS State Code}
 #' \item{usps}{Official United States Postal Service (USPS) Code}
 #' \item{state}{State Name}
@@ -10,3 +11,17 @@
 #'
 #' @source \url{https://www.census.gov/geo/reference/ansi_statetables.html}
 "fips"
+
+#' State-level FIPS Code
+#'
+#' FIPS Codes for the States and the District of Columbia form Census.
+#'
+#' @format A data frame with three variables:
+#' \describe{
+#' \item{fips}{FIPS State Code}
+#' \item{usps}{Official United States Postal Service (USPS) Code}
+#' \item{state}{State Name}
+#' }
+#'
+#' @source \url{https://www.census.gov/geo/reference/ansi_statetables.html}
+"state"
