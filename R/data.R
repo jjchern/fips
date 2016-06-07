214b7e4a1cd1e75e898e59b26a8cbed58ac279d5
jjchern <jiajia.chern@gmail.com>
2016-06-07 09:06:25 -0500

    Simplify documentation

--- R/data.R
+++ R/data.R
@@ -2,13 +2,6 @@
 #'
 #' FIPS Codes for the 50 states, the District of Columbia, and the Outlying Areas of the United States.
 #'
-#' @format A data frame with three variables:
-#' \describe{
-#' \item{fips}{FIPS State Code}
-#' \item{usps}{Official United States Postal Service (USPS) Code}
-#' \item{state}{State Name}
-#' }
-#'
 #' @source \url{https://www.census.gov/geo/reference/ansi_statetables.html}
 "fips"
 
@@ -16,28 +9,12 @@
 #'
 #' FIPS Codes for the 50 States and the District of Columbia.
 #'
-#' @format A data frame with three variables:
-#' \describe{
-#' \item{fips}{FIPS State Code}
-#' \item{usps}{Official United States Postal Service (USPS) Code}
-#' \item{state}{State Name}
-#' }
-#'
 #' @source \url{https://www.census.gov/geo/reference/ansi_statetables.html}
 "state"
 
 #' County-level FIPS Code
 #'
 #' 2010 FIPS Codes for Counties and County Equivalent Entities.
-#'
-#' @format A data frame with five variables:
-#' \describe{
-#' \item{usps}{Official United States Postal Service (USPS) Code}
-#' \item{state}{State Name}
-#' \item{fips}{2010 FIPS County Code}
-#' \item{county}{Full County Name}
-#' \item{cty_short}{Short County Name}
-#' }
 #'
 #' @source \url{https://www.census.gov/geo/reference/codes/cou.html}
 "county"
